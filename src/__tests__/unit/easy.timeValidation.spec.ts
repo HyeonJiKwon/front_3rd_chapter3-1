c289a03f98c915173151f1e140ed0ff9c12257ed
khjbest39 <khjbest39@golfzon.com>
2024-11-04 12:13:11 +0900

    feat:easy.timeValidation.spec 작성완료

--- src/__tests__/unit/easy.timeValidation.spec.ts
+++ src/__tests__/unit/easy.timeValidation.spec.ts
@@ -1,15 +1,69 @@
-import { getTimeErrorMessage } from '../../utils/timeValidation';
+import { getTimeErrorMessage, TimeValidationResult } from '../../utils/timeValidation';
 
-describe('getTimeErrorMessage >', () => {
-  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {});
+describe('getTimeErrorMessage', () => {
+  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {
+    const start = '14:00';
+    const end = '13:00';
+    const expected: TimeValidationResult = {
+      startTimeError: '시작 시간은 종료 시간보다 빨라야 합니다.',
+      endTimeError: '종료 시간은 시작 시간보다 늦어야 합니다.',
+    };
+    const result = getTimeErrorMessage(start, end);
+    expect(result).toEqual(expected);
+  });
 
-  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {});
+  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {
+    const start = '12:00';
+    const end = '12:00';
+    const expected: TimeValidationResult = {
+      startTimeError: '시작 시간은 종료 시간보다 빨라야 합니다.',
+      endTimeError: '종료 시간은 시작 시간보다 늦어야 합니다.',
+    };
+    const result = getTimeErrorMessage(start, end);
+    expect(result).toEqual(expected);
+  });
 
-  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {
+    const start = '09:00';
+    const end = '10:00';
+    const expected: TimeValidationResult = {
+      startTimeError: null,
+      endTimeError: null,
+    };
+    const result = getTimeErrorMessage(start, end);
+    expect(result).toEqual(expected);
+  });
 
-  it('시작 시간이 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간이 비어있을 때 null을 반환한다', () => {
+    const start = '';
+    const end = '10:00';
+    const expected: TimeValidationResult = {
+      startTimeError: null,
+      endTimeError: null,
+    };
+    const result = getTimeErrorMessage(start, end);
+    expect(result).toEqual(expected);
+  });
 
-  it('종료 시간이 비어있을 때 null을 반환한다', () => {});
+  it('종료 시간이 비어있을 때 null을 반환한다', () => {
+    const start = '09:00';
+    const end = '';
+    const expected: TimeValidationResult = {
+      startTimeError: null,
+      endTimeError: null,
+    };
+    const result = getTimeErrorMessage(start, end);
+    expect(result).toEqual(expected);
+  });
 
-  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {
+    const start = '';
+    const end = '';
+    const expected: TimeValidationResult = {
+      startTimeError: null,
+      endTimeError: null,
+    };
+    const result = getTimeErrorMessage(start, end);
+    expect(result).toEqual(expected);
+  });
 });
